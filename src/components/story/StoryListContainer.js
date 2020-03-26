92efedefad78e8dde62fd052462503d92e0abb4f
David Z. Williams <dave@flex.io>
2020-03-26 16:57:06 -0400

    Added background color. Removed 'add' and 'remove' test buttons.

--- src/components/story/StoryListContainer.js
+++ src/components/story/StoryListContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { StoryList } from './StoryList'
 import {
   addStory,
@@ -9,32 +9,10 @@ import {
 import styles from './Story.module.css'
 
 export function StoryListContainer() {
-  const dispatch = useDispatch()
   const items = useSelector(selectItems)
-  const len = items.length
-  const newStory = {
-    id: len,
-    title: `Item ${len+1}`
-  }
 
   return (
     <div className={styles.container}>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Add story"
-          onClick={() => dispatch(addStory(newStory))}
-        >
-          Add story
-        </button>
-        <button
-          className={styles.button}
-          aria-label="Remove story"
-          onClick={() => dispatch(removeStory(items.length - 1))}
-        >
-          Remove story
-        </button>
-      </div>
       <StoryList items={items} />
     </div>
   )
